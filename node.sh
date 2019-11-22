83f11f323b51cc0cd770d9dfe0c7ca5cf5d2143d
Sandor Ferentzi <Sandor Ferentzi>
2019-11-22 02:25:32 +0100

    add sudo to node.sh

--- node.sh
+++ node.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-yum install git -y
-curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
-yum install gcc-c++ make nodejs -y
-yum install -y nodejs
-node index.js
+sudo yum install git -y
+sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
+sudo yum install gcc-c++ make nodejs -y
+sudo yum install -y nodejs
+sudo node index.js
 #cd /vagrant_data
 # sudo npm install -g pm2
 #npm install supervisor -g
